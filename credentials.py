6b7acc0a0dad26321ea83041c190eaffb9b6f208
kingsam91 <lambetgm@gmail.com>
2020-07-08 06:19:14 +0300

    define credentials class and init

---
+++ credentials.py
@@ -0,0 +1,11 @@
+class Credential:
+    """
+    a class to generate credential for users
+    """
+    pass
+    credential_array = []
+
+    def __init__(self, user_name, password, email):
+        self.user_name = user_name
+        self.password = password
+        self.email = email
