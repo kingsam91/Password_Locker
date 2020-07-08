5c145cf2d40f395fe6074ef7b62f65192e4a60d8
kingsam91 <lambetgm@gmail.com>
2020-07-08 06:08:55 +0300

    define methods that will apply

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3.6
+def save_user():
+    
+def create_user(first,last,email):
+
+def create_credentials(user_name,password,email):
+
+def save_credentials():
+
+def display_user_cred():
+
+def del_user_cred():
+
+
 
 def main():
     print("dsafdsgf")
