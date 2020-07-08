1cdd7be745e928015177b5e7c83f24f8aeef176a
kingsam91 <lambetgm@gmail.com>
2020-07-08 06:13:04 +0300

    define user class and init

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+     """
+    a class that generates new instances of users
+    """
+
+    user_list = []
+
+    def __init__ = (self, first,last,email):
+        self.first = first
+        self.last = 
+        # self.phone_number = phone_number
+        self.email = email
